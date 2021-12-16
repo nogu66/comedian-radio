c36b1608ecf6b0f56da220f1a9b994a78e79a235
Yuta <ezochi66@gmail.com>
2021-12-16 14:12:41 +0900

    トップページのレスポンシブ

--- src/components/BodyCard.jsx
+++ src/components/BodyCard.jsx
@@ -8,58 +8,58 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-    cMedia: {
-      height: 0,
-      paddingTop: '56.25%',
-    },
-    cContent: {
-      height: '50px',
-      overflow: 'hidden',
-    },
-    cColor:{
-      // fontWeight:'bold',
-      color:'#666666'
-    }
-  });
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  cMedia: {
+    height: 0,
+    paddingTop: '56.25%',
+  },
+  cContent: {
+    height: '50px',
+    overflow: 'hidden',
+  },
+  cColor: {
+    // fontWeight:'bold',
+    color: '#666666',
+  },
+});
 
-  function BodyCard(props) {
-    // const {id, name, date, time, hp_url, station_id, twitter_hashtag, twitter_url, talents } = props;
-    const name = props.program.name;
+function BodyCard(props) {
+  // const {id, name, date, time, hp_url, station_id, twitter_hashtag, twitter_url, talents } = props;
+  const name = props.program.name;
 
-    const classes = useStyles();
-    return (
-      <Card variant="outlined" className={classes.card}>
-        {/* <CardMedia style={{ height: '150px' }} image={imageUrl} /> */}
-        <CardMedia
-          style={{height:'10px' }}
-          image={"https://radiko.jp/res/program/DEFAULT_IMAGE/LFR/2uvc37bx9r.png"}
-          // alt={name}
-          className={classes.cMedia}
-        />
-        <CardContent className={classes.cContent}>
-          <Typography variant="body1" component="p" className={classes.cColor}>
-            {name}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          {/* <Button size="small" href={`/post/${id}`}> */}
-          <Button size="small" className={classes.cColor} >
-            詳細をみる
-          </Button>
-        </CardActions>
-      </Card>
-    );
-  }
+  const classes = useStyles();
+  return (
+    <Card variant="outlined" className={classes.card}>
+      {/* <CardMedia style={{ height: '150px' }} image={imageUrl} /> */}
+      <CardMedia
+        style={{ height: '10px' }}
+        image={'https://radiko.jp/res/program/DEFAULT_IMAGE/LFR/2uvc37bx9r.png'}
+        // alt={name}
+        className={classes.cMedia}
+      />
+      <CardContent className={classes.cContent}>
+        <Typography variant="body1" component="p" className={classes.cColor}>
+          {name}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {/* <Button size="small" href={`/post/${id}`}> */}
+        <Button size="small" className={classes.cColorC}>
+          詳細をみる
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
 
-  export default BodyCard;
+export default BodyCard;
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,54 +1,55 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core';
-import { db } from "../firebase/index";
-import useMedia from "use-media";
+import { db } from '../firebase/index';
+import useMedia from 'use-media';
 import { Grid } from '@material-ui/core';
 import BodyCard from './BodyCard';
 
 const useStyles = makeStyles({
-    contents: {
-      marginTop: '20px'
-    }
-  });
+  contents: {
+    marginTop: '20px',
+  },
+});
 
-  function Content() {
-    const classes = useStyles();
-    const [programs, setPrograms] = useState([]);
-    const isWide = useMedia({minWidth: "742px"})
-    useEffect(() => {
-        db.collection('radioPrograms').get().then((snapshots) => {
-            const programs = [];
-            snapshots.forEach((doc) => {
-                const data = doc.data();
-                // console.log(data);
-                programs.push({
-                    id: doc.id,
-                    name: data.name,
-                    date: data.date,
-                    time: data.time,
-                    hp_url: data.hp_url,
-                    station_id: data.station_id,
-                    twitter_hashtag: data.twitter_hashtag,
-                    twitter_url: data.twitter_url,
-                    talnets: data.talents,
-                })
-            })
-            setPrograms(programs);
-        })
-    }, []);
+function Content() {
+  const classes = useStyles();
+  const [programs, setPrograms] = useState([]);
+  const isWide = useMedia({ minWidth: '742px' });
+  useEffect(() => {
+    db.collection('radioPrograms')
+      .get()
+      .then((snapshots) => {
+        const programs = [];
+        snapshots.forEach((doc) => {
+          const data = doc.data();
+          // console.log(data);
+          programs.push({
+            id: doc.id,
+            name: data.name,
+            date: data.date,
+            time: data.time,
+            hp_url: data.hp_url,
+            station_id: data.station_id,
+            twitter_hashtag: data.twitter_hashtag,
+            twitter_url: data.twitter_url,
+            talnets: data.talents,
+          });
+        });
+        setPrograms(programs);
+      });
+  }, []);
 
-    return (
-      <Grid container spacing={1} className={classes.contents}>
-        {programs.map((program, index) => {
-          return (
-            <Grid item xs={12} sm={isWide ? 4 : 6}>
-              <BodyCard program={program}/>
-            </Grid>
-          );
-        })}
-      </Grid>
-    );
+  return (
+    <Grid container spacing={1} className={classes.contents}>
+      {programs.map((program, index) => {
+        return (
+          <Grid item xs={12} sm={isWide ? 4 : 6}>
+            <BodyCard program={program} />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
 
-  };
-
-  export default Content;
+export default Content;
